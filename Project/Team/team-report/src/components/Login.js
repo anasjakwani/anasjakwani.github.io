41a616af83461defb07d350c1f74b7ab095b2dd9
anasjakwani <64756595+anasjakwani@users.noreply.github.com>
2021-10-16 21:51:21 +0500

    cha

---
+++ Project/Team/team-report/src/components/Login.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export const Login = () => {
+  return (
+    <div className="d-flex justify-content-center">
+      <form className=" w-50 p-3">
+        <div className="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div className="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
